17c473ac8851ec47810d7646f4fa47bdfb83000c
FelipeGallego <gallegofelipe29@gmail.com>
2024-04-24 16:11:25 -0500

    creacion proyecto

---
+++ Data/ProjectShiftsContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using ProjectShifts.Models;
+namespace ProjectShifts.Data
+{
+    public class ProjectShiftsContext : DbContext
+    {
+        public ProjectShiftsContext(DbContextOptions<ProjectShiftsContext> options) : base (options)
+        {
+
+        }
+        public DbSet <Administrador> Administrador { get; set;}
+        public DbSet <Usuarios> Usuarios { get; set;}
+        public DbSet <Turnos> Turnos { get; set;}
+        public DbSet <TipoDocumento> TipoDocumento { get; set;}
+        public DbSet <TipoTurno> TipoTurno { get; set;}
+
+
+    }
+}
---
+++ Models/TipoTurno.cs
@@ -0,0 +1,8 @@
+namespace ProjectShifts.Models
+{
+    public class TipoTurno
+    {
+        public int Id { get; set;}
+        public string Nombre { get; set; }
+    }
+}
